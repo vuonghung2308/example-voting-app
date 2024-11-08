48cdb95152fdb6a92eab65277b13c75319c81169
Vương Mạnh Hùng <51767162+vuonghung2308@users.noreply.github.com>
2024-11-08 13:56:32 +0700

    Update app.py

--- vote/app.py
+++ vote/app.py
@@ -8,6 +8,7 @@ import logging
 
 option_a = os.getenv('OPTION_A', "Cats")
 option_b = os.getenv('OPTION_B', "Dogs")
+passwd = os.getenv('REDIS_PASS', 'password')
 hostname = socket.gethostname()
 
 app = Flask(__name__)
@@ -18,7 +19,7 @@ app.logger.setLevel(logging.INFO)
 
 def get_redis():
     if not hasattr(g, 'redis'):
-        g.redis = Redis(host="redis", db=0, socket_timeout=5)
+        g.redis = Redis(host="redis", db=0, password=passwd, socket_timeout=5)
     return g.redis
 
 @app.route("/", methods=['POST','GET'])
